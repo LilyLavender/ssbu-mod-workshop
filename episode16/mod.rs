907fbd6a94537c2e1d3632c7128c324d71c87c12
LilyLambda <102371110+LilyLavender@users.noreply.github.com>
2023-05-29 15:18:12 -0500

    Added episode16

---
+++ episode16/mod.rs
@@ -0,0 +1,51 @@
+use {
+    smash::{
+        lua2cpp::*,
+        phx::*,
+        app::{sv_animcmd::*, lua_bind::*, *},
+        lib::lua_const::*,
+		hash40
+    },
+    smash_script::*,
+    smashline::*,
+};
+
+#[acmd_script( agent = "ganon", script = "game_attack11", category = ACMD_GAME, low_priority )]
+unsafe fn ganon_attack11(fighter: &mut L2CAgentBase) {
+    frame(fighter.lua_state_agent, 3.0);
+    macros::FT_MOTION_RATE(fighter, 0.8);
+    frame(fighter.lua_state_agent, 8.0);
+    macros::FT_MOTION_RATE(fighter, 1.0);
+    if macros::is_excute(fighter) {
+        macros::ATTACK(fighter, 0, 0, Hash40::new("top"), 11.0, 361, 74, 0, 41, 4.4, 0.0, 12.0, 11.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_elec"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_HEAVY, *ATTACK_REGION_PUNCH);
+        macros::ATTACK(fighter, 1, 0, Hash40::new("top"), 11.0, 361, 74, 0, 41, 5.0, 0.0, 12.0, 19.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_elec"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_HEAVY, *ATTACK_REGION_PUNCH);
+        macros::ATTACK(fighter, 2, 0, Hash40::new("top"), 11.0, 361, 74, 0, 41, 3.5, 0.0, 12.0, 7.0, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_F, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_HEAVY, *ATTACK_REGION_PUNCH);
+		shield!(fighter, *MA_MSC_CMD_REFLECTOR, *COLLISION_KIND_REFLECTOR, 
+		0, // ID
+		Hash40::new("top"), // Bone
+		7.5, // Size
+		0.0, 12.0, 23.0, // X1, Y1, Z1
+		0.0, 12.0, 23.0, // X2, Y2, Z2
+		4.0, // Damage multiplier
+		0.1, // Speed multiplier
+		200, // Max damage
+		false, // Unknown
+		20.0, // Life multiplier
+		*FIGHTER_REFLECTOR_GROUP_HOMERUNBAT); // "type"
+	}
+    wait(fighter.lua_state_agent, 2.0);
+    if macros::is_excute(fighter) {
+        AttackModule::clear_all(fighter.module_accessor);
+    }
+	frame(fighter.lua_state_agent, 14.0);
+    if macros::is_excute(fighter) {
+		shield!(fighter, *MA_MSC_CMD_SHIELD_OFF, *COLLISION_KIND_REFLECTOR, 0, *FIGHTER_REFLECTOR_GROUP_HOMERUNBAT);
+    }
+    macros::FT_MOTION_RATE(fighter, 0.9);
+}
+
+pub fn install() {
+    smashline::install_acmd_scripts!(
+        ganon_attack11,
+    );
+}
