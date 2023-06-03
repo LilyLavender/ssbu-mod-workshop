05b970e71cc0912a81709d1deec6018a6efbb8c6
LilyLambda <102371110+LilyLavender@users.noreply.github.com>
2023-06-03 10:50:33 -0500

    Added episode25

---
+++ episode25/mod.rs
@@ -0,0 +1,28 @@
+use {
+    smash::{
+        lua2cpp::*,
+        phx::*,
+        app::{sv_animcmd::*, lua_bind::*, *},
+        lib::lua_const::*,
+		hash40
+    },
+    smash_script::*,
+    smashline::*
+};
+
+#[skyline::hook(replace = StatusModule::situation_kind)]
+unsafe fn situation_kind_replace(module_accessor: &mut smash::app::BattleObjectModuleAccessor) -> i32 {
+    if PostureModule::pos_x(module_accessor) < 0.0 {
+		
+		return *SITUATION_KIND_GROUND;
+		
+	} 
+	
+	original!()(module_accessor)
+}
+
+pub fn install() {
+	skyline::install_hooks!(
+        situation_kind_replace
+    );
+}
