9d286f3b53f1591f8c882c8b7258f453abf7ca1e
LilyLambda <102371110+LilyLavender@users.noreply.github.com>
2023-06-06 15:30:17 -0500

    Updated episode28

--- episode28/mod.rs
+++ episode28/mod.rs
@@ -12,15 +12,21 @@ use {
 
 #[skyline::hook(replace = sv_animcmd::ATTACK)]
 unsafe fn attack_replace(lua_state: u64) {
+	// Declare boma and fighter_kind from lua_state parameter
     let boma = smash::app::sv_system::battle_object_module_accessor(lua_state);
     let fighter_kind = smash::app::utility::get_kind(boma);
 	
+	// Declare l2c_agent from lua_state parameter
     let mut l2c_agent = L2CAgent::new(lua_state);
+	// Declare hitbox_params as a growable array from stack we're getting from lua_state
     let hitbox_params: Vec<L2CValue> = (0..36).map(|i| l2c_agent.pop_lua_stack(i + 1)).collect();
-    l2c_agent.clear_lua_stack();
+    // Clear stack
+	l2c_agent.clear_lua_stack();
+	// Repeat 36 times
     for i in 0..36 {
         if fighter_kind == FIGHTER_KIND_BUDDY {
 			
+			// If index of for loop is a specific number, set it to a certain value
 			if i == 3 {
 				l2c_agent.push_lua_stack(&mut L2CValue::new_num(10.0));
 			} else if i == 7 {
@@ -28,6 +34,7 @@ unsafe fn attack_replace(lua_state: u64) {
 			} else if i == 32 {
 				l2c_agent.push_lua_stack(&mut L2CValue::new_int(80658106433));
 			}
+			// If index isn't one of the aforementioned values, use the values originally passed into the function
 			else {
 				l2c_agent.push_lua_stack(&mut hitbox_params[i].clone());
 			}
