ef2b091120a641abc03428f3cbc7160da95d60cc
LilyLambda <102371110+LilyLavender@users.noreply.github.com>
2023-10-02 11:10:37 -0500

    Better formatted pre script

--- episode30/mod.rs
+++ episode30/mod.rs
@@ -37,26 +37,30 @@ unsafe extern "C" fn ridley_specialhi_main_loop(fighter: &mut L2CFighterCommon) 
 
 #[status_script(agent = "ridley", status = FIGHTER_STATUS_KIND_SPECIAL_HI, condition = LUA_SCRIPT_STATUS_FUNC_STATUS_PRE)]
 unsafe fn ridley_specialhi_pre(fighter: &mut L2CFighterCommon) -> L2CValue {
-	StatusModule::init_settings(fighter.module_accessor, 
-	smash::app::SituationKind(*SITUATION_KIND_NONE),
-	*FIGHTER_KINETIC_TYPE_UNIQ,
-	GROUND_CORRECT_KIND_KEEP.into(),
-	smash::app::GroundCliffCheckKind(*GROUND_CLIFF_CHECK_KIND_ON_DROP_BOTH_SIDES),
-	true,
-	*FIGHTER_STATUS_WORK_KEEP_FLAG_NONE_FLAG,
-	*FIGHTER_STATUS_WORK_KEEP_FLAG_NONE_INT,
-	*FIGHTER_STATUS_WORK_KEEP_FLAG_NONE_FLOAT,
-	0);
-	FighterStatusModuleImpl::set_fighter_status_data(fighter.module_accessor, 
-	false, 
-	*FIGHTER_TREADED_KIND_NO_REAC,
-	false, 
-	false, 
-	false, 
-	(*FIGHTER_LOG_MASK_FLAG_ATTACK_KIND_SPECIAL_HI | *FIGHTER_LOG_MASK_FLAG_ACTION_CATEGORY_ATTACK | *FIGHTER_LOG_MASK_FLAG_ACTION_TRIGGER_ON) as u64,
-	FIGHTER_STATUS_ATTR_START_TURN.into(),
-	FIGHTER_POWER_UP_ATTACK_BIT_SPECIAL_HI.into(),
-	0);
+	StatusModule::init_settings(
+		fighter.module_accessor, 
+		smash::app::SituationKind(*SITUATION_KIND_NONE),
+		*FIGHTER_KINETIC_TYPE_UNIQ,
+		GROUND_CORRECT_KIND_KEEP.into(),
+		smash::app::GroundCliffCheckKind(*GROUND_CLIFF_CHECK_KIND_ON_DROP_BOTH_SIDES),
+		true,
+		*FIGHTER_STATUS_WORK_KEEP_FLAG_NONE_FLAG,
+		*FIGHTER_STATUS_WORK_KEEP_FLAG_NONE_INT,
+		*FIGHTER_STATUS_WORK_KEEP_FLAG_NONE_FLOAT,
+		0
+	);
+	FighterStatusModuleImpl::set_fighter_status_data(
+		fighter.module_accessor, 
+		false, 
+		*FIGHTER_TREADED_KIND_NO_REAC,
+		false, 
+		false, 
+		false, 
+		(*FIGHTER_LOG_MASK_FLAG_ATTACK_KIND_SPECIAL_HI | *FIGHTER_LOG_MASK_FLAG_ACTION_CATEGORY_ATTACK | *FIGHTER_LOG_MASK_FLAG_ACTION_TRIGGER_ON) as u64,
+		FIGHTER_STATUS_ATTR_START_TURN.into(),
+		FIGHTER_POWER_UP_ATTACK_BIT_SPECIAL_HI.into(),
+		0
+	);
 	return 0.into();
 }
 
