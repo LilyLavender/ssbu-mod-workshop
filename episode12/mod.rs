1d749f01d1c879af8e63d02ddb8a3c2fe0eb0fda
LilyLambda <102371110+LilyLavender@users.noreply.github.com>
2023-10-26 11:47:52 -0500

    Added comments, removed parentheses

--- episode12/mod.rs
+++ episode12/mod.rs
@@ -14,11 +14,11 @@ use {
 fn elight_frame(fighter: &mut L2CFighterCommon) {
     unsafe {
 
-		if (MotionModule::motion_kind(fighter.module_accessor) == hash40("attack_s3s") || 
+		if MotionModule::motion_kind(fighter.module_accessor) == hash40("attack_s3s") || 
 		MotionModule::motion_kind(fighter.module_accessor) == hash40("attack_hi3") || 
-		MotionModule::motion_kind(fighter.module_accessor) == hash40("attack_lw3")) {
+		MotionModule::motion_kind(fighter.module_accessor) == hash40("attack_lw3") {
 			
-			if MotionModule::frame(fighter.module_accessor) > 10.0 && MotionModule::frame(fighter.module_accessor) < 20.0{
+			if MotionModule::frame(fighter.module_accessor) > 10.0 && MotionModule::frame(fighter.module_accessor) < 20.0 {
 				
 				if ControlModule::check_button_on(fighter.module_accessor, *CONTROL_PAD_BUTTON_JUMP) {
 					
@@ -28,13 +28,14 @@ fn elight_frame(fighter: &mut L2CFighterCommon) {
 				
 			}
 			
-		} else if (MotionModule::motion_kind(fighter.module_accessor) == hash40("attack_air_b") || 
+		} else if MotionModule::motion_kind(fighter.module_accessor) == hash40("attack_air_b") || 
 					MotionModule::motion_kind(fighter.module_accessor) == hash40("attack_air_f") || 
 					MotionModule::motion_kind(fighter.module_accessor) == hash40("attack_air_n") || 
 					MotionModule::motion_kind(fighter.module_accessor) == hash40("attack_air_hi") || 
-					MotionModule::motion_kind(fighter.module_accessor) == hash40("attack_air_lw")) {
-			
-			if (FighterMotionModuleImpl::get_cancel_frame(fighter.module_accessor, Hash40::new_raw( MotionModule::motion_kind(fighter.module_accessor)), false) - MotionModule::frame(fighter.module_accessor) <= 20.0) {
+					MotionModule::motion_kind(fighter.module_accessor) == hash40("attack_air_lw") {
+
+			// Removes 20 frames of endlag by allowing a cancel if the current frame is 20 frames or fewer away from the normal cancel frame
+			if FighterMotionModuleImpl::get_cancel_frame(fighter.module_accessor, Hash40::new_raw(MotionModule::motion_kind(fighter.module_accessor)), false) - MotionModule::frame(fighter.module_accessor) <= 20.0 {
 				
 				CancelModule::enable_cancel(fighter.module_accessor);
 				
@@ -50,6 +51,7 @@ unsafe fn elight_attack11(fighter: &mut L2CAgentBase) {
     frame(fighter.lua_state_agent, 1.0);
     macros::FT_MOTION_RATE(fighter, 0.5);
     frame(fighter.lua_state_agent, 3.0);
+	// motion_rate originally 1.0
     macros::FT_MOTION_RATE(fighter, 3.0);
     if macros::is_excute(fighter) {
         macros::ATTACK(fighter, 0, 0, Hash40::new("top"), 2.0, 361, 20, 0, 15, 2.5, 0.0, 8.0, 8.5, None, None, None, 1.0, 1.0, *ATTACK_SETOFF_KIND_ON, *ATTACK_LR_CHECK_POS, false, 0, 0.0, 0, false, false, false, false, true, *COLLISION_SITUATION_MASK_GA, *COLLISION_CATEGORY_MASK_ALL, *COLLISION_PART_MASK_ALL, false, Hash40::new("collision_attr_normal"), *ATTACK_SOUND_LEVEL_S, *COLLISION_SOUND_ATTR_PUNCH, *ATTACK_REGION_PUNCH);
