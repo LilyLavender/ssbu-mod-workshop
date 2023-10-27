9156356a0063d0a5218c08aa1f823220c8a8c13d
LilyLambda <102371110+LilyLavender@users.noreply.github.com>
2023-10-27 12:29:44 -0500

    Added episode34

---
+++ episode34/game.lua
@@ -0,0 +1,39 @@
+﻿game_Throw = function ()
+    if sv_animcmd.is_excute() then
+        sv_animcmd.ATTACK_FP(0, 0, 13402447818, 1, 361, 100, 0, 0, 5, 0, 0, 0, 92925133491, 0, 1, 1, false, false, 0, ATTACK_SOUND_LEVEL_S, COLLISION_SOUND_ATTR_PUNCH, COLLISION_SITUATION_MASK_GA, false, ATTACK_REGION_NONE, COLLISION_CATEGORY_MASK_NO_STAGE, false, COLLISION_PART_MASK_ALL, false, false, true, false, ITEM_TRWATK_F, false, false, ATTACK_LR_CHECK_POS, false, false, false, false, false, COLLISION_SHAPE_TYPE_SPHERE)
+        AttackModule.enable_safe_pos()
+    end
+    return 
+end
+
+game_ThrowLioleusboss = function ()
+    if sv_animcmd.is_excute() then
+        sv_animcmd.ATTACK_FP(0, 0, 13402447818, 0, 361, 0, 0, 0, 5, 0, 0, 0, 92925133491, 0, 0, 0, false, false, 0, ATTACK_SOUND_LEVEL_S, COLLISION_SOUND_ATTR_NONE, COLLISION_SITUATION_MASK_GA, false, ATTACK_REGION_NONE, COLLISION_CATEGORY_MASK_NO_STAGE, false, COLLISION_PART_MASK_ALL, false, false, true, false, ITEM_TRWATK_F, false, false, ATTACK_LR_CHECK_POS, false, false, false, false, true, COLLISION_SHAPE_TYPE_SPHERE)
+        AttackModule.enable_safe_pos()
+    end
+    return 
+end
+
+-- THIS FUNCTION IS EDITED
+game_Born = function ()
+    if sv_animcmd.is_excute() then
+        sv_animcmd.IT_SET_PARENT_RUMBLE(72656470004, 0)
+		-- Arguments are id, part, bone, dmg, ang, kbg, fkb, bkb, size, x, y, z, collision_attr, etc...
+        sv_animcmd.ATTACK_FP(0, 0, 13402447818, 12, 90, 80, 0, 50, 20, 0, 0, 0, 0x149cdc52bb, 0, 1, 1, false, false, 0, ATTACK_SOUND_LEVEL_L, COLLISION_SOUND_ATTR_PUNCH, COLLISION_SITUATION_MASK_A, false, ATTACK_REGION_NONE, COLLISION_CATEGORY_MASK_NO_STAGE, false, COLLISION_PART_MASK_ALL, false, false, true, false, ITEM_TRWATK_F, false, false, ATTACK_LR_CHECK_POS, false, false, false, false, false, COLLISION_SHAPE_TYPE_SPHERE)
+        sv_animcmd.ATTACK_FP(1, 0, 13402447818, 12, 90, 100, 300, 0, 20, 0, 0, 0, 0x149cdc52bb, 0, 4, 1, false, false, 0, ATTACK_SOUND_LEVEL_L, COLLISION_SOUND_ATTR_PUNCH, COLLISION_SITUATION_MASK_G, false, ATTACK_REGION_NONE, COLLISION_CATEGORY_MASK_NO_STAGE, false, COLLISION_PART_MASK_ALL, false, false, true, false, ITEM_TRWATK_F, false, false, ATTACK_LR_CHECK_POS, false, false, false, false, false, COLLISION_SHAPE_TYPE_SPHERE)
+        AttackModule.set_no_finish_camera_ex(1, true, false)
+    end
+    return 
+end
+
+game_BornLioleusboss = function ()
+    if sv_animcmd.is_excute() then
+        sv_animcmd.IT_SET_PARENT_RUMBLE(72656470004, 0)
+        sv_animcmd.ATTACK_FP(0, 0, 13402447818, 0.10000000149011612, 90, 80, 0, 50, 20, 0, 0, 0, 84299446750, 0, 1, 1, false, false, 0, ATTACK_SOUND_LEVEL_L, COLLISION_SOUND_ATTR_PUNCH, COLLISION_SITUATION_MASK_A, false, ATTACK_REGION_NONE, COLLISION_CATEGORY_MASK_NO_STAGE, false, COLLISION_PART_MASK_ALL, false, false, true, false, ITEM_TRWATK_F, false, false, ATTACK_LR_CHECK_POS, false, false, false, false, false, COLLISION_SHAPE_TYPE_SPHERE)
+        sv_animcmd.ATTACK_FP(1, 0, 13402447818, 0.10000000149011612, 90, 100, 300, 0, 20, 0, 0, 0, 84299446750, 0, 4, 1, false, false, 0, ATTACK_SOUND_LEVEL_L, COLLISION_SOUND_ATTR_PUNCH, COLLISION_SITUATION_MASK_G, false, ATTACK_REGION_NONE, COLLISION_CATEGORY_MASK_NO_STAGE, false, COLLISION_PART_MASK_ALL, false, false, true, false, ITEM_TRWATK_F, false, false, ATTACK_LR_CHECK_POS, false, false, false, false, false, COLLISION_SHAPE_TYPE_SPHERE)
+        AttackModule.set_no_finish_camera_ex(1, true, false)
+    end
+    return 
+end
+
+return 
---
+++ episode34/mod.rs
@@ -0,0 +1,24 @@
+use {
+    smash::{
+        lua2cpp::*,
+        phx::*,
+        app::{sv_animcmd::*, lua_bind::*, *},
+        lib::lua_const::*,
+		hash40
+    },
+    smash_script::*,
+    smashline::*,
+};
+
+#[acmd_script( agent = "younglink", scripts = [ "game_appealhil", "game_appealhir", "game_appealsl", "game_appealsr", "game_appeallwl", "game_appeallwr" ], category = ACMD_GAME, low_priority )]
+unsafe fn younglink_game_appeal(agent: &mut L2CAgentBase) {
+    if macros::is_excute(agent) {
+        ItemModule::have_item(agent.module_accessor, smash::app::ItemKind(*ITEM_KIND_DEKU), 0, 0, false, false);
+    }
+}
+
+pub fn install() {
+    smashline::install_acmd_scripts!(
+        younglink_game_appeal,
+    );
+}
