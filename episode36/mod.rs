94214610e9f1539f232a59a76286646b364e15e4
LilyLambda <102371110+LilyLavender@users.noreply.github.com>
2023-10-27 20:17:08 -0500

    Added episode36

---
+++ episode36/mod.rs
@@ -0,0 +1,28 @@
+use {
+    smash::{
+        lua2cpp::*,
+        phx::*,
+        app::{sv_animcmd::*, lua_bind::*, *},
+        lib::lua_const::*,
+		hash40
+    },
+    smash_script::*,
+    smashline::*,
+};
+
+#[fighter_frame_callback]
+pub fn global_fighter_frame(fighter : &mut L2CFighterCommon) {
+    unsafe {
+		let boma = fighter.module_accessor;
+		if StatusModule::status_kind(boma) == *FIGHTER_STATUS_KIND_ATTACK_HI4_HOLD && 
+		   ControlModule::check_button_on(fighter.module_accessor, *CONTROL_PAD_BUTTON_JUMP) {
+				fighter.change_status(FIGHTER_STATUS_KIND_JUMP.into(), false.into());
+		}
+    }
+}
+
+pub fn install() {
+    smashline::install_agent_frame_callbacks!(
+		global_fighter_frame
+    );
+}
