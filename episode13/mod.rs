b03c438bd3bb82d1261f235f3414ec6fc57e4230
LilyLambda <102371110+LilyLavender@users.noreply.github.com>
2023-10-27 20:19:46 -0500

    Added comments

--- episode13/mod.rs
+++ episode13/mod.rs
@@ -9,18 +9,20 @@ use {
     smash_script::*,
     smashline::*,
 };
-
+// Allows for variable to be tracked for every player seperately
 static mut paluPosX: [f32; 8] = [0.0; 8];
 static mut paluPosY: [f32; 8] = [0.0; 8];
 static mut paluPosZ: [f32; 8] = [0.0; 8];
 
 #[acmd_script( agent = "palutena", scripts = [ "game_appealhil", "game_appealhir" ], category = ACMD_GAME, low_priority )]
 unsafe fn palutena_appealhi(fighter: &mut L2CAgentBase) {
+	// Get player number
     let entry_id = WorkModule::get_int(fighter.module_accessor, *FIGHTER_INSTANCE_WORK_ID_INT_ENTRY_ID) as usize;
 	if macros::is_excute(fighter) {
         KineticModule::set_consider_ground_friction(fighter.module_accessor, false, *KINETIC_ENERGY_RESERVE_ATTRIBUTE_MAIN);
         ArticleModule::generate_article(fighter.module_accessor, *FIGHTER_PALUTENA_GENERATE_ARTICLE_GODWING, false, -1);
         ArticleModule::change_motion(fighter.module_accessor, *FIGHTER_PALUTENA_GENERATE_ARTICLE_GODWING, Hash40::new("appeal_hi_l"), false, -1.0);
+		// Sets position
 		PostureModule::set_pos(fighter.module_accessor, &Vector3f{ x: paluPosX[entry_id], y: paluPosY[entry_id], z: paluPosZ[entry_id] });
 	}
 }
