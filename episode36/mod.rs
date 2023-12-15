cb9c32d704a7b70f8803de7e306f540e3648755c
LilyLambda <102371110+LilyLavender@users.noreply.github.com>
2023-12-15 13:29:21 -0600

    Why was only one instance of fighter.module_accessor replaced with the boma that I had just defined? Couldn't tell ya!

--- episode36/mod.rs
+++ episode36/mod.rs
@@ -15,7 +15,7 @@ pub fn global_fighter_frame(fighter : &mut L2CFighterCommon) {
     unsafe {
 		let boma = fighter.module_accessor;
 		if StatusModule::status_kind(boma) == *FIGHTER_STATUS_KIND_ATTACK_HI4_HOLD && 
-		   ControlModule::check_button_on(fighter.module_accessor, *CONTROL_PAD_BUTTON_JUMP) {
+		   ControlModule::check_button_on(boma, *CONTROL_PAD_BUTTON_JUMP) {
 				fighter.change_status(FIGHTER_STATUS_KIND_JUMP.into(), false.into());
 		}
     }
