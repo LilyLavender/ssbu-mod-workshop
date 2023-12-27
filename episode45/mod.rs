e569cfc2c15ead5fd5690cdc71ddd4a17f009144
LilyLambda <102371110+LilyLavender@users.noreply.github.com>
2023-12-27 16:04:10 -0600

    Split command input list into its own file

--- episode45/mod.rs
+++ episode45/mod.rs
@@ -17,12 +17,15 @@ pub const CHECK_SPECIAL_COMMAND:	i32 = 0x3C;
 
 const FIGHTER_WOLF_INSTANCE_WORK_ID_FLAG_SPECIAL_S_COMMAND : i32 = 0x200000f0;
 
+// Agent init - Runs once when a fighter starts existing
 #[fighter_init]
 fn agent_init(fighter: &mut L2CFighterCommon) {
 	unsafe {
 		let fighter_kind = fighter.global_table[KIND].get_i32();
 		if fighter_kind == *FIGHTER_KIND_WOLF {
+			// Turning this flag on allows a character to do command inputs
 			WorkModule::on_flag(fighter.module_accessor, *FIGHTER_INSTANCE_WORK_ID_FLAG_CAN_SPECIAL_COMMAND);
+			// Assigning our own defined function to the correct spot in the global_table
 			fighter.global_table[CHECK_SPECIAL_COMMAND].assign(&L2CValue::Ptr(wolf_check_special_command as *const () as _));
 		}
 	}
@@ -33,7 +36,10 @@ unsafe extern "C" fn wolf_check_special_command(fighter: &mut L2CFighterCommon) 
 	let cat4 = fighter.global_table[CMD_CAT4].get_i32();
 	
 	// Side B
+	// If player did a qcf input
+	// A full list of inputs can be found in the other file in the episode45 folder.
 	if !ret && cat4 & *FIGHTER_PAD_CMD_CAT4_FLAG_SPECIAL_N_COMMAND != 0
+	// If fighter is allowed to transition into side b (ie, not in hitstun, not doing another attack)
 	&& WorkModule::is_enable_transition_term(fighter.module_accessor, *FIGHTER_STATUS_TRANSITION_TERM_ID_CONT_SPECIAL_S_COMMAND) {
 		WorkModule::on_flag(fighter.module_accessor, FIGHTER_WOLF_INSTANCE_WORK_ID_FLAG_SPECIAL_S_COMMAND);
 		fighter.change_status(FIGHTER_STATUS_KIND_SPECIAL_S.into(), false.into());
@@ -41,7 +47,9 @@ unsafe extern "C" fn wolf_check_special_command(fighter: &mut L2CFighterCommon) 
 	}
 	
 	// Up B
+	// If player did a [2]8A/B input
 	if !ret && cat4 & *FIGHTER_PAD_CMD_CAT4_FLAG_SPECIAL_HI2_COMMAND != 0
+	// If fighter is allowed to transition into up b
 	&& WorkModule::is_enable_transition_term(fighter.module_accessor, *FIGHTER_STATUS_TRANSITION_TERM_ID_CONT_SPECIAL_HI_COMMAND) {
 		fighter.change_status(FIGHTER_STATUS_KIND_SPECIAL_HI.into(), true.into());
 		ret = true;
@@ -51,6 +59,7 @@ unsafe extern "C" fn wolf_check_special_command(fighter: &mut L2CFighterCommon) 
 	ret.into()
 }
 
+// Add a section to a status script without having to translate it
 #[status_script(agent = "wolf", status = FIGHTER_STATUS_KIND_SPECIAL_S, condition = LUA_SCRIPT_STATUS_FUNC_STATUS_MAIN)]
 unsafe extern "C" fn wolf_special_s_start_main(fighter: &mut L2CFighterCommon) -> L2CValue {
     if WorkModule::is_flag(fighter.module_accessor, FIGHTER_WOLF_INSTANCE_WORK_ID_FLAG_SPECIAL_S_COMMAND) {
@@ -68,43 +77,3 @@ pub fn install() {
 		wolf_special_s_start_main,
 	);
 }
-
-// List from black_calculus & LilyLambda, with help from WuBoy
-// All of these can be inputted in the air. Ones with back inputs typically don't work well for characters without autoturnaround
-
-/*
-// Ryu/Ken/Terry Command Inputs
-	FIGHTER_PAD_CMD_CAT4_FLAG_SPECIAL_N_COMMAND = Quarter Circle Forward, Hadoken [ 236A/B ]
-	FIGHTER_PAD_CMD_CAT4_FLAG_SPECIAL_N2_COMMAND = Half Circle Forward, Shakunetsu Hadoken [ 41236A/B ]
-	FIGHTER_PAD_CMD_CAT4_FLAG_SPECIAL_S_COMMAND = Quarter Circle Back, Tatsumaki Senpukyaku [ 214A/B ]
-	FIGHTER_PAD_CMD_CAT4_FLAG_SPECIAL_HI_COMMAND = Z input, Shoryuken [ 623A/B ]
-	FIGHTER_PAD_CMD_CAT4_FLAG_SPECIAL_HI2_COMMAND = Charge down -> Up, Rising Tackle [ [2]8A/B ]
-	FIGHTER_PAD_CMD_CAT4_FLAG_SUPER_SPECIAL_COMMAND = Dobule Quarter Circle Forward, Buster Wolf [ 236236A/B ]
-	FIGHTER_PAD_CMD_CAT4_FLAG_SUPER_SPECIAL_R_COMMAND = Dobule Quarter Circle Back, Buster Wolf [ 214214A/B ]
-	FIGHTER_PAD_CMD_CAT4_FLAG_SUPER_SPECIAL2_COMMAND = Half Circle Back -> Forward, Power Geyser [ 21416A/B, 2426A/B, 2146A/B ]
-	FIGHTER_PAD_CMD_CAT4_FLAG_SUPER_SPECIAL2_R_COMMAND = Half Circle Forward -> Back, Power Geyser [ 23634A/B, 2624A/B, 2364A/B ]
-	FIGHTER_PAD_CMD_CAT4_FLAG_ATTACK_COMMAND1 = Quarter circle down, Nata Otoshi Geri [ 632A/B ]
-
-// Kazuya Command Inputs
-	FIGHTER_PAD_CMD_CAT4_FLAG_COMMAND_6N6AB = Double tap forward, Left Splits Kick [ 656A ]
-	FIGHTER_PAD_CMD_CAT4_FLAG_COMMAND_623NB = Crouch Dash [ 623 ]
-	FIGHTER_PAD_CMD_CAT4_FLAG_COMMAND_323CATCH = Gates of Hell [ 323Z ]
-	FIGHTER_PAD_CMD_CAT4_FLAG_COMMAND_623A = Wind God Fist [ 623A ]
-	FIGHTER_PAD_CMD_CAT4_FLAG_COMMAND_623STRICT = Electric Wind God Fist [ 623#A ]
-	FIGHTER_PAD_CMD_CAT4_FLAG_COMMAND_623ALONG = Dragon Uppercut / Rage Drive [ 623[A] ]
-	FIGHTER_PAD_CMD_CAT4_FLAG_COMMAND_623BLONG = Rage Drive [ 623[B] ]
-
-// Other
-	FIGHTER_PAD_CMD_CAT4_FLAG_COMMAND_6N6 = Double tap forward (no buttons) [ 656 ]
-	FIGHTER_PAD_CMD_CAT4_FLAG_COMMAND_4N4 = Double tap backward (no buttons) [ 454 ]
-
-// Numpad Inputs (Don't work)
-	FIGHTER_PAD_CMD_CAT4_FLAG_COMMAND_1 = Numpad 1, Kazuya's Stature Smash / Crouch Spin Kick
-	FIGHTER_PAD_CMD_CAT4_FLAG_COMMAND_2 = Numpad 2, Kazuya's Neijiri Uraken / Crouch Jab
-	FIGHTER_PAD_CMD_CAT4_FLAG_COMMAND_3 = Numpad 3, Kazuya's Tsunami Kick / Tombstone Crusher
-	FIGHTER_PAD_CMD_CAT4_FLAG_COMMAND_4 = Numpad 4, Kazuya's Flash Tornado
-	FIGHTER_PAD_CMD_CAT4_FLAG_COMMAND_6 = Numpad 6, Kazuya's Oni Front Kick
-	FIGHTER_PAD_CMD_CAT4_FLAG_COMMAND_7 = Numpad 7, Kazuya's Jump Side Kick
-	FIGHTER_PAD_CMD_CAT4_FLAG_COMMAND_8 = Numpad 8, Kazuya's Twin Pistons
-	FIGHTER_PAD_CMD_CAT4_FLAG_COMMAND_9 = Numpad 9, Kazuya's Roundhouse to Triple Spin Kicks
-*/
