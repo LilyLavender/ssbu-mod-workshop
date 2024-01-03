9cc339615c79ca25606a5c08af629ff2094ef86f
LilyLambda <102371110+LilyLavender@users.noreply.github.com>
2024-01-02 22:46:39 -0600

    Tab fixes 1

--- episode11/no-special-fall.rs
+++ episode11/no-special-fall.rs
@@ -1,28 +1,28 @@
 use {
-  smash::{
-      lua2cpp::*,
-      phx::*,
-      app::{sv_animcmd::*, lua_bind::*, *},
-      lib::{lua_const::*, L2CValue, L2CAgent},
-      hash40
-  },
-  smash_script::*,
-  smashline::*
+    smash::{
+        lua2cpp::*,
+        phx::*,
+        app::{sv_animcmd::*, lua_bind::*, *},
+        lib::{lua_const::*, L2CValue, L2CAgent},
+        hash40
+    },
+    smash_script::*,
+    smashline::*
 };
 
 #[fighter_frame( agent = FIGHTER_KIND_KAMUI )]
 fn kamui_frame(fighter: &mut L2CFighterCommon) {
-  unsafe {
-      // No special fall
-      if MotionModule::motion_kind(fighter.module_accessor) == hash40("fall_special") {
-          fighter.change_status(FIGHTER_STATUS_KIND_FALL.into(), false.into());
-          WorkModule::set_int(fighter.module_accessor, 1, *FIGHTER_INSTANCE_WORK_ID_INT_JUMP_COUNT);
-      }
-  }
+    unsafe {
+        // No special fall
+        if MotionModule::motion_kind(fighter.module_accessor) == hash40("fall_special") {
+            fighter.change_status(FIGHTER_STATUS_KIND_FALL.into(), false.into());
+            WorkModule::set_int(fighter.module_accessor, 1, *FIGHTER_INSTANCE_WORK_ID_INT_JUMP_COUNT);
+        }
+    }
 }
 
 pub fn install() {
-  smashline::install_agent_frames!(
-      kamui_frame
-  );
+    smashline::install_agent_frames!(
+        kamui_frame
+    );
 }
