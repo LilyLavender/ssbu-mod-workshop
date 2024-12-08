ea5560590c92341ec533364f68b166aa43211171
LilyLambda <102371110+LilyLavender@users.noreply.github.com>
2024-12-08 08:52:54 -0600

    Align with most recent changes in ArticleCloningGuide.md

--- episode46/lib.rs
+++ episode46/lib.rs
@@ -18,5 +18,5 @@ mod chrom;
 #[skyline::main(name = "smashline_test")]
 pub fn main() {
     chrom::install();
-    smashline::clone_weapon("mario", "fireball", "chrom", "lightbeam", true);
+    smashline::clone_weapon("mario", *smash::lib::lua_const::WEAPON_KIND_MARIO_FIREBALL, "chrom", "lightbeam", true);
 }
